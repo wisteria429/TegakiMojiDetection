71c7f6b48bc2ace97eb9549171566b3e9c47befe
fuji <wisteriaroots429@gmail.com>
2019-01-06 23:55:02 +0900

    [TensorFlow] modelとラベルを読み込む処理を実装
    MainActivityで仮呼び出し

--- app/src/main/java/jp/study/fuji/tegakimojidetection/MainActivity.kt
+++ app/src/main/java/jp/study/fuji/tegakimojidetection/MainActivity.kt
@@ -2,9 +2,13 @@ package jp.study.fuji.tegakimojidetection
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +16,12 @@ class MainActivity : AppCompatActivity() {
         bt_clear.setOnClickListener {
             canvas.clear()
         }
+
+        val loader = TfAssetLoader(assets)
+        val labels = loader.loadTfLabelList()
+        Log.v(TAG, labels[0] + ":"+ labels.size)
+
+        val mapper = loader.loadTfModelFile()
+        Log.v(TAG, mapper.toString())
     }
 }
---
+++ app/src/main/java/jp/study/fuji/tegakimojidetection/TfAssetLoader.kt
@@ -0,0 +1,38 @@
+package jp.study.fuji.tegakimojidetection
+
+import android.app.Activity
+import android.content.res.AssetManager
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+
+class TfAssetLoader(val assets:AssetManager) {
+    companion object {
+        private const val MODEL_PATH = "my_model.lite"
+        private const val LABEL_PATH = "my_labels.txt"
+    }
+
+    fun loadTfModelFile() : MappedByteBuffer {
+        val fileDescriptor = assets.openFd(MODEL_PATH)
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel = inputStream.channel;
+
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,
+            fileDescriptor.startOffset, fileDescriptor.declaredLength)
+    }
+
+    fun loadTfLabelList() : List<String> {
+        val labelList = arrayListOf<String>()
+
+        BufferedReader(InputStreamReader(assets.open(LABEL_PATH))).use {
+            while(true) {
+                val line:String = it.readLine() ?: break
+                labelList.add(line)
+            }
+        }
+
+        return labelList
+    }
+}
